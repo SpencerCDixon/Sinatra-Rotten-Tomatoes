017a3cca54602c8cbb9f0286966cc9061d8cf1a7
Spencer Dixon <spencercdixon@gmail.com>
2014-08-15 14:33:03 -0400

    sdlkf

--- app.rb
+++ app.rb
@@ -29,16 +29,28 @@ post '/movies/:id' do
   @review = params[:review]
   @time = Time.now.strftime("%Y/%m/%d At %H:%M%p")
   @id = params[:id]
+  @review_id = params[:review_id]
 
   CSV.open("reviews.csv","ab") do |row|
-    row << [@id,@name,@review,@time.to_s]
+    row << [@review_id,@id,@name,@review,@time.to_s]
   end
   CSV.foreach("reviews.csv",headers:true) do |row|
-   reviews <<  {id: row["ID"], name: row["NAME"], review: row["REVIEW"], time: row["TIME"]}
+   reviews <<  {review_id: row["REVIEW_ID"],id: row["ID"], name: row["NAME"], review: row["REVIEW"], time: row["TIME"]}
   end
   redirect '/'
 end
 
-get '/movies/:id/review' do
-  "hello world"
+# Delete Reviews
+get '/movies/:id/review/:review_id' do
+  erb :delete
+end
+
+delete '/movies/:id/review/:review_id' do
+  CSV.foreach("reviews.csv", headers:true) do |row|
+    row.delete_if{ row[:review_id] == params[:review_id]}
+  end
+  CSV.open("reviews.csv", 'w',headers:true) do |row|
+   reviews <<  {review_id: row["REVIEW_ID"],id: row["ID"], name: row["NAME"], review: row["REVIEW"], time: row["TIME"]}
+  end
+  redirect '/'
 end
